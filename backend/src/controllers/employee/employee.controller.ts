d823e76afb59fdb0f130104a8a70ef74d2e98d41
thiagolemos23 <111323385+thiagolemos23@users.noreply.github.com>
2024-06-13 20:18:16 -0300

    Update employee.controller.ts

--- backend/src/controllers/employee/employee.controller.ts
+++ backend/src/controllers/employee/employee.controller.ts
@@ -2,32 +2,4 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EmployeeService } from '../../services/employee/employee.service';
 import { Funcionario } from '@prisma/client'; 
 
-@Controller('funcionarios')
-export class EmployeeController {
-  constructor(private readonly funcionarioService: EmployeeService) {}
 
-  @Post()
-  create(@Body() data: Funcionario) {
-    return this.funcionarioService.create(data);
-  }
-
-  @Get()
-  findAll() {
-    return this.funcionarioService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.funcionarioService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() data: Funcionario) {
-    return this.funcionarioService.update(id, data);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.funcionarioService.remove(id);
-  }
-}
