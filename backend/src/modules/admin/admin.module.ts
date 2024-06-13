2f2c66c84b99075e2969f9b2297bcb7006f1e8ea
samuelkaina <146041413+samuelkaina@users.noreply.github.com>
2024-06-13 20:20:23 -0300

    Update admin.module.ts

--- backend/src/modules/admin/admin.module.ts
+++ backend/src/modules/admin/admin.module.ts
@@ -3,3 +3,9 @@ import { AdminController } from 'src/controllers/admin/admin.controller';
 import { AdminService } from 'src/services/admin/admin.service';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 
+@Module({
+    providers: [AdminService, PrismaService],
+    controllers: [AdminController],
+    exports: [AdminService],
+})
+export class AdminModule {}
