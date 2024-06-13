800142a51a492b27da1c452b271cf4bfbc7378b9
samuelkaina <146041413+samuelkaina@users.noreply.github.com>
2024-06-13 20:18:49 -0300

    Update epi.controller.ts

--- backend/src/controllers/epi/epi.controller.ts
+++ backend/src/controllers/epi/epi.controller.ts
@@ -2,3 +2,32 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EpiService } from '../../services/epi/epi.service';
 import { EPI } from '@prisma/client';
 
+@Controller('epi')
+export class EpiController {
+  constructor(private readonly epiService: EpiService) {}
+
+  @Post()
+  create(@Body() data: EPI) {
+    return this.epiService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.epiService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.epiService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: EPI) {
+    return this.epiService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.epiService.remove(id);
+  }
+}
