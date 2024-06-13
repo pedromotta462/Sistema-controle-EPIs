fbb2bc43c4547ecb7045df98c8cc98d46a9ae2fc
samuelkaina <146041413+samuelkaina@users.noreply.github.com>
2024-06-13 20:17:09 -0300

    Update admin.controller.ts

--- backend/src/controllers/admin/admin.controller.ts
+++ backend/src/controllers/admin/admin.controller.ts
@@ -10,3 +10,32 @@ import {
 import { AdminService } from '../../services/admin/admin.service';
 import { Admin } from '@prisma/client';
 
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post()
+  create(@Body() data: Admin) {
+    return this.adminService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.adminService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.adminService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: Admin) {
+    return this.adminService.update(id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.adminService.remove(id);
+  }
+}
